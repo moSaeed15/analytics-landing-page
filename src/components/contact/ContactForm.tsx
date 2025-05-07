99297515e59db3636771a4fccbc2e23878552500
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2025-05-07 16:30:40 +0300

    finished contact form

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,7 +1,74 @@
-import React from 'react';
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import CustomFormField from './FormRender';
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: 'Name must be at least 2 characters.',
+  }),
+  email: z.string().email({ message: 'Invalid email address.' }),
+  message: z.string().min(10, {
+    message: 'Message must be at least 10 characters.',
+  }),
+});
 
 const ContactForm = () => {
-  return <form className=""></form>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-grey rounded-4xl py-10 px-7"
+      >
+        <CustomFormField
+          control={form.control}
+          name="name"
+          label="Name"
+          placeholder="Name"
+          type="input"
+        />
+        <CustomFormField
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="Email"
+          type="input"
+        />
+        <CustomFormField
+          control={form.control}
+          name="message"
+          label="Message"
+          placeholder="Message"
+          type="textarea"
+        />
+      </form>
+      <Button
+        type="submit"
+        className="min-h-16 bg-dark text-white w-full rounded-2xl  py-5 text-xl mt-7 cursor-pointer hover:bg-dark/90 "
+      >
+        Send Message
+      </Button>
+    </Form>
+  );
 };
 
 export default ContactForm;
---
+++ src/components/contact/FormRender.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormField,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Control, ControllerRenderProps } from 'react-hook-form';
+import { Textarea } from '../ui/textarea';
+
+type field = ControllerRenderProps<
+  {
+    name: string;
+    email: string;
+    message: string;
+  },
+  'name' | 'email' | 'message'
+>;
+
+type FormRenderProps = {
+  label: string;
+  type: 'input' | 'textarea';
+  placeholder: string;
+  field: field;
+};
+
+const FormRender = ({ field, label, type, placeholder }: FormRenderProps) => {
+  return (
+    <FormItem>
+      <FormLabel>{label}</FormLabel>
+      <FormControl>
+        {type === 'input' ? (
+          <Input
+            placeholder={placeholder}
+            {...field}
+            className="bg-white py-5 px-5 placeholder:text-[#898989] border-black"
+          />
+        ) : (
+          <Textarea
+            placeholder={placeholder}
+            {...field}
+            className="bg-white py-5 px-5 placeholder:text-[#898989] border-black"
+          />
+        )}
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  );
+};
+
+type CustomFormFieldProps = {
+  control: Control<{
+    message: string;
+    name: string;
+    email: string;
+  }>;
+  name: 'name' | 'message' | 'email';
+  type: 'input' | 'textarea';
+  placeholder: string;
+  label: string;
+};
+
+export const CustomFormField = ({
+  control,
+  name,
+  type,
+  label,
+  placeholder,
+}: CustomFormFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormRender
+          field={field}
+          label={label}
+          type={type}
+          placeholder={placeholder}
+        />
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
